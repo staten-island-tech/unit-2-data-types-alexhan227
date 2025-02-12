649cbe193f22d716669769e014da97ddedd261bb
alexhan227 <alexanderh281@nycstudents.net>
2025-02-12 09:24:27 -0500

    Day 7

--- challenge2.py
+++ challenge2.py
@@ -1,17 +1,23 @@
 bill = int(input("How much was the bill?: "))
 tip = input("Was the service bad, okay, good, or great?: ")
+badtip = (bill * 0)
 okaytip = (bill * .15)
 goodtip = (bill * .20)
 greattip = (bill * .25)
 
-tipquestion1 = input("do you want to tip: ")
+# asks if want to tip
+tipquestion1 = input("Do you want to tip? (yes or no): ")
 
-
-
-if tipquestion1 == ("yes"):
-    print(okaytip)
-elif ("no"):
+# If they say yes, ask about the service rating
+if tipquestion1 == "yes":
+    if tip == "okay":
+        print(f"Your tip is: ${okaytip:.2f}")
+        #individuallizing the qeustion allows the code to fufuill ONE conditional at a time
+    elif tip == "good":
+        print(f"Your tip is: ${goodtip:.2f}")
+    elif tip == "great":
+        print(f"Your tip is: ${greattip:.2f}")
+    elif tip == "bad":
+        print(f"Your tip is: ${badtip:.2f}")
+elif tipquestion1 == "no":
     print("Have a nice day, come again!")
-if tip == ("okay"):
-    tipquestion1
-
---
+++ challenge3.py
@@ -0,0 +1,2 @@
+number = input("enter a number")
+
