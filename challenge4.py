bf930f623f15d2b2b78e532c28212a5bc0c436ae
alexhan227 <alexanderh281@nycstudents.net>
2025-02-25 02:06:48 +0000

    Day 8 success

--- challenge4.py
+++ challenge4.py
@@ -4,7 +4,7 @@ def gcd(a, b):
     return a  # The last non-zero remainder is the GCF
 
 # Example usage:
-num1 = input("enter a number: ")
-num2 = input("enter another number: ")
+num1 = int(input("enter a number: "))
+num2 = int(input("enter another number: "))
 result = gcd(num1, num2)
 print(f"The GCF of {num1} and {num2} is {result}")
