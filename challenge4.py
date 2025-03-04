a0e85e6aacd8ce87fdac9c87c43def225ae4095e
alexhan227 <alexanderh281@nycstudents.net>
2025-03-04 12:51:38 +0000

    Day idk

--- challenge4.py
+++ challenge4.py
@@ -1,9 +1,34 @@
 num1 = int(input("enter a number: "))
 num2 = int(input("enter another number: "))
 
-def find_factors(number):
-    for i in range(1, number + 1):
-        if number % i == 0:  
+factorsA = []
+factorsB = []
+def find_factorsB(num1):
+    for i in range(1, num1 + 1):
+        if num1 % i == 0:  
+            factorsB.append(i)
+
+def find_factorsA(num2):
+    for i in range(1, num2 + 1):
+        if num2 % i == 0:  
+            factorsA.append(i)
 
 
-print(f"Factors of {num1}: {factors}")
+def GCF(factorsA, factorsB):
+    common_factors = []  # List to store common factors
+    
+    # Find common factors between the two lists
+    for i in factorsA:
+        if i in factorsB:
+            common_factors.append(i)  # Append the common factor to the list
+    
+    # Manually find the largest common factor
+    gcf = 1  # Start with a default value of 1
+    for factor in common_factors:
+        if factor > gcf:
+            gcf = factor  # Update the GCF to the largest common factor found
+
+    return gcf
+
+gcf = GCF(factorsA, factorsB)
+print(f"The GCF is: {gcf}")
